d10ba85336ad7b35b025d813b38ce715371f5f1e
mostfasafi <106494729+mostfasafi@users.noreply.github.com>
2024-09-29 15:50:02 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,14 +10,18 @@ var x = setInterval(function () {
   var distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24) - 1920);
+  var hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) + 1
+  );
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML =
-    "يوم " + days + "ساعه" + hours + "دقائق" + minutes + "ثواني " + seconds;
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
 
   // If the count down is finished, write some text
   if (distance < 0) {
